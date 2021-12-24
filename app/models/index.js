1b0e1df31f757133364b55b699e304f173595d2a
Keeghan McGarry <keeghan_m@live.com>
2021-12-24 19:15:05 +0000

    Environment Variables

--- app/models/index.js
+++ app/models/index.js
@@ -2,14 +2,13 @@ const dbConfig = require("../config/db.config.js");
 
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize({
-    dialect: dbConfig.dialect,
-    username: dbConfig.username,
-    password: dbConfig.password,
-    host: dbConfig.host,
-    port: dbConfig.port,
-    database: dbConfig.database,
-    dialectOptions: dbConfig.dialectOptions,
-    logging: dbConfig.logging,
+    dialect: 'postgres',
+    username: process.env.PUZZLE_DB_USERNAME,
+    password: process.env.PUZZLE_DB_PWORD,
+    host: process.env.PUZZLE_DB_HOST,
+    port: process.env.PUZZLE_DB_PORT,
+    database: process.env.PUZZLE_DB_DB,
+    logging: false,
   });
 
 const db = {};
