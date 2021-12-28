8c78315197b81df0196b8128a36494bd60f19b0a
Keeghan McGarry <keeghan_m@live.com>
2021-12-28 21:42:49 +0000

    Changed array handling

--- app/controllers/puzzle.controller.js
+++ app/controllers/puzzle.controller.js
@@ -6,10 +6,8 @@ const { sequelize } = require("../models");
 const {QueryTypes} = require('sequelize');
 const db = require("../models");
 const Puzzle = db.puzzles;
-const Op = db.Sequelize.Op;
 
 exports.mainAccess = async (req,res) => {
-
   if(req.headers["x-mashape-proxy-secret"] == undefined || req.headers["x-mashape-proxy-secret"] != process.env.RapidAPISecret) {
     res.status(400).send("Request must be sent via RapidAPI") 
     return
@@ -17,7 +15,7 @@ exports.mainAccess = async (req,res) => {
 
   let puzzles 
   let queryString = "SELECT puzzleid,fen,rating,ratingdeviation,moves,themes FROM puzzles WHERE 1=1 "
-  let secStr = ["/",";","'",'"']
+  let secStr = ["/",";"]
   
   // First check if no query string present
   // if this is the case, return a single random puzzle
@@ -74,35 +72,40 @@ exports.mainAccess = async (req,res) => {
       queryString += "AND array_length(moves,1) = " +(parseInt(req.query.playerMoves)*2) + " "
     }
 
-    // If multiple themes are specified, they also need to specify a type
     if(req.query.themes) {
-      if(req.query.themesType || typeof(req.query.themes) == "string") {
       queryString += "AND ("
 
-      if(typeof(req.query.themes) == "string") {
-        queryString += " '" +req.query.themes+ "'=ANY(themes)"
-      } else {
-        for(let theme of req.query.themes){
-          queryString += " '" +theme+ "'=ANY(themes)"
-          queryString += req.query.themesType == "OR" ? " OR" : " AND" // the type sets whether we use AND or OR selectors for the theme
-        }
-        queryString = queryString.substring(0, queryString.lastIndexOf(" "));
+      // First extract the themes from the query string
+      let themes
+      try {
+        themes = JSON.parse(req.query.themes)
       }
-      queryString += ") "
+      catch{
+        res.status(400).send("Invalid Themes Array")
+      }
 
-      } else {
-        console.log("in")
-        res.status(400).send("Invalid Query String")
+      // If multiple themes are specified, they also need to specify a type
+      if(themes.length > 1 && !req.query.themesType) {
+        res.status(400).send("themesType needed when multiple themes supplied")
         return
       }
+
+      // Now loop through the themes and attach them
+      for(let theme of themes){
+        queryString += " '" +theme+ "'=ANY(themes)"
+        queryString += req.query.themesType == "OR" ? " OR" : " AND" // the type sets whether we use AND or OR selectors for the theme
+      }
+      queryString = queryString.substring(0, queryString.lastIndexOf(" "));
+      queryString += ") "
     }
-    
+      
     // We always want to attach a rating (to make the query faster) and a limit
     // If the user hasn't supplied a rating, we will generate one for them
     let limit = req.query.count ? req.query.count : 1
     let rating = req.query.rating ? req.query.rating : Math.floor(Math.random() * (3001 - 511 + 1) + 511);
     queryString += "AND rating BETWEEN " + (parseInt(rating)) + "-ratingdeviation AND " + (parseInt(rating)) + "+ratingdeviation ORDER BY RANDOM() LIMIT "+limit
 
+    try {
     puzzles = await sequelize.query(
       queryString,
       {
@@ -110,15 +113,18 @@ exports.mainAccess = async (req,res) => {
         model: Puzzle,
         mapToModel: true
       });
+    }
+    catch {
+      res.status(500).send("Error fetching puzzles. Please contact the admin.")
+      return
+    }
 
   }
-  
 
   if(puzzles.length == 0){
     res.status(400).send("No Matching Puzzles")
     return
   }
-
   res.status(200).send({
     puzzles
   })
